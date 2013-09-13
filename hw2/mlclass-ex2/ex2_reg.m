ca44b80e8d99a741589d4d79dfe0259c5cb0da9c
waldron <waldron@5750G>
2013-09-13 20:31:28 +0800

    a

--- hw2/mlclass-ex2/ex2_reg.m
+++ hw2/mlclass-ex2/ex2_reg.m
@@ -87,7 +87,7 @@ pause;
 initial_theta = zeros(size(X, 2), 1);
 
 % Set regularization parameter lambda to 1 (you should vary this)
-lambda = 1;
+lambda = 0;
 
 % Set Options
 options = optimset('GradObj', 'on', 'MaxIter', 400);
@@ -114,3 +114,4 @@ p = predict(theta, X);
 fprintf('Train Accuracy: %f\n', mean(double(p == y)) * 100);
 
 
+pause;
